3de0d3ef81df40c03d3f273837d6fb24e6e4b66d
yossi lerer <ylerer770@gmail.com>
2025-11-02 12:31:28 +0200

    add all projecy

--- MissionControl.py
+++ MissionControl.py
@@ -6,12 +6,12 @@ class MissionControl:
     def analyze_report( r: Report):
 
         if r.urgency_level <= 4:
-            print("Immediate response required.")
+            return "Immediate response required."
 
         elif r.urgency_level == 3:
-            print("High priority. Monitor closely.")
+            return "High priority. Monitor closely."
 
         else:
-            print("High priority. Monitor closely.")
+            return "High priority. Monitor closely."
 
 
--- main.py
+++ main.py
@@ -4,9 +4,21 @@ from IntelTools import  IntelTools
 from Report import Report
 from MissionControl import MissionControl
 
-mission = Mission("YY", "TLV")
-mission.assigned_agent1(Agent("yt", 9))
+# mission = Mission("YY", "TLV")
+# mission.assigned_agent1(Agent("yt", 9))
+#
+# mission.brief()
+#
+# agent1 = Agent("unit 8200", 4)
+#
+# d = IntelTools()
+# print(d.encrypt_message("yossi"))
+# d.log_transmission("yossi", "i'm in hermon class")
 
-mission.brief()
+agent =Agent("yossi", 6)
 
-agent1 = Agent("unit 8200", 4)
+report = Report("dachuf meod", 4)
+report.submitted_by1(agent)
+
+MissionControl.analyze_report(report)
+IntelTools.log_transmission(agent.code_name, MissionControl.analyze_report(report))
